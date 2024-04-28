34a6559cc11f9e266c5dfb24dd5b6d0a0eb2ad36
lotus <thamaraiselvig2109@gmail.com>
2024-04-28 21:15:35 +0530

    post-application

---
+++ src/features/Postapp/ShowPost.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import { useSelector } from "react-redux";
+import { SelectAllPost } from "./PostSlice";
+import TimeAge from "./TimeAge";
+
+
+const ShowPost = () => {
+  const posts = useSelector(SelectAllPost);
+  const renderPosts = posts.map((data) => (
+    <div
+      key={data.id}
+      className="my-2 row-bd  text-start p-3 "
+    >
+      <h6>{data.title}</h6>
+      <p>By {data.author} - <TimeAge timeStamp={data.date}/></p>
+      <p>{data.content.substring(0,200)}</p>
+    </div>
+  ));
+
+  return (
+    <div className="py-5">
+      <h3>Latest Posts :</h3>
+      <div className="row pt-3">{renderPosts}</div>
+    </div>
+  );
+};
+
+export default ShowPost;
